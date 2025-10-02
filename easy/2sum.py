931980a22ffc423b8d8ea8835de21179a618595b
Anil Bronson <anilbronson12@gmail.com>
2025-10-02 16:20:50 -0700

    first commit

---
+++ easy/2sum.py
@@ -0,0 +1,17 @@
+from typing import List
+
+# Given an array of integers nums and an integer target, 
+# return indices of the two numbers such that they add up to target.
+# You may assume that each input would have exactly one solution, 
+# and you may not use the same element twice.
+
+# O(n^2) solution
+def twoSum(self, nums: List[int], target: int) -> List[int]:
+    for n in range(len(nums)):
+        for i in range(n + 1, len(nums)):
+            if nums[n] + nums[i] == target:
+                return [n, i]
+    return []
+
+def twoSum(self, nums: List[int], target: int) -> List[int]:
+    
